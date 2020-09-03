8e356cd440b42b91c2b706adc27bb5af58ab89ba
zaid <khanzaid786786.kz@gmail.com>
2020-09-03 22:02:06 +0530

    Updates

---
+++ precache-manifest.9fa451a2fd7a0321c97381187e67099d.js
@@ -0,0 +1,378 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c27219a924f52f9a163caa097bc810",
+    "url": "/otopark_superadmin/index.html"
+  },
+  {
+    "revision": "544d3558807908f41277",
+    "url": "/otopark_superadmin/static/css/1.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "13f9bcac20965cb7e58e",
+    "url": "/otopark_superadmin/static/css/10.2e715660.chunk.css"
+  },
+  {
+    "revision": "e115381e825e985e6fce",
+    "url": "/otopark_superadmin/static/css/11.2e715660.chunk.css"
+  },
+  {
+    "revision": "c75f4952ed054b935745",
+    "url": "/otopark_superadmin/static/css/14.2e715660.chunk.css"
+  },
+  {
+    "revision": "09626177fc2408330d11",
+    "url": "/otopark_superadmin/static/css/15.2e715660.chunk.css"
+  },
+  {
+    "revision": "6850fca47afc542dbb9f",
+    "url": "/otopark_superadmin/static/css/16.2e715660.chunk.css"
+  },
+  {
+    "revision": "689291a86ae45ff611fe",
+    "url": "/otopark_superadmin/static/css/17.2e715660.chunk.css"
+  },
+  {
+    "revision": "fe19c6d82ca4583164f5",
+    "url": "/otopark_superadmin/static/css/18.2e715660.chunk.css"
+  },
+  {
+    "revision": "32130ade87b49cf00508",
+    "url": "/otopark_superadmin/static/css/19.2e715660.chunk.css"
+  },
+  {
+    "revision": "b56034162f76d1b9d4c3",
+    "url": "/otopark_superadmin/static/css/20.2e715660.chunk.css"
+  },
+  {
+    "revision": "b0e6c17b44c722af2a2b",
+    "url": "/otopark_superadmin/static/css/21.2e715660.chunk.css"
+  },
+  {
+    "revision": "4700253434b3104c9c43",
+    "url": "/otopark_superadmin/static/css/9.2e715660.chunk.css"
+  },
+  {
+    "revision": "9c77fc76900d7ff01bad",
+    "url": "/otopark_superadmin/static/js/0.2828f3f5.chunk.js"
+  },
+  {
+    "revision": "544d3558807908f41277",
+    "url": "/otopark_superadmin/static/js/1.a28bdba2.chunk.js"
+  },
+  {
+    "revision": "c726b1dbff91e7b5455bd2ccab525212",
+    "url": "/otopark_superadmin/static/js/1.a28bdba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f9bcac20965cb7e58e",
+    "url": "/otopark_superadmin/static/js/10.2fea774a.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/otopark_superadmin/static/js/10.2fea774a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e115381e825e985e6fce",
+    "url": "/otopark_superadmin/static/js/11.0b31a6e0.chunk.js"
+  },
+  {
+    "revision": "57ea35da2f835d94d693d49e80508ef6",
+    "url": "/otopark_superadmin/static/js/11.0b31a6e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc363f6ca65996f31fac",
+    "url": "/otopark_superadmin/static/js/12.a6cef2a2.chunk.js"
+  },
+  {
+    "revision": "0f5e6f1b5203b86ab1202ae62480dbf2",
+    "url": "/otopark_superadmin/static/js/12.a6cef2a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55123ecdf2f6961bb7ca",
+    "url": "/otopark_superadmin/static/js/13.734a22a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/otopark_superadmin/static/js/13.734a22a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75f4952ed054b935745",
+    "url": "/otopark_superadmin/static/js/14.ed2206f4.chunk.js"
+  },
+  {
+    "revision": "09626177fc2408330d11",
+    "url": "/otopark_superadmin/static/js/15.cfe4e3ae.chunk.js"
+  },
+  {
+    "revision": "6850fca47afc542dbb9f",
+    "url": "/otopark_superadmin/static/js/16.3ae1633f.chunk.js"
+  },
+  {
+    "revision": "689291a86ae45ff611fe",
+    "url": "/otopark_superadmin/static/js/17.326f67fc.chunk.js"
+  },
+  {
+    "revision": "fe19c6d82ca4583164f5",
+    "url": "/otopark_superadmin/static/js/18.b6c37e59.chunk.js"
+  },
+  {
+    "revision": "32130ade87b49cf00508",
+    "url": "/otopark_superadmin/static/js/19.428d12c7.chunk.js"
+  },
+  {
+    "revision": "2858a55250769a81fa12",
+    "url": "/otopark_superadmin/static/js/2.5eb2634e.chunk.js"
+  },
+  {
+    "revision": "b56034162f76d1b9d4c3",
+    "url": "/otopark_superadmin/static/js/20.f06e9754.chunk.js"
+  },
+  {
+    "revision": "b0e6c17b44c722af2a2b",
+    "url": "/otopark_superadmin/static/js/21.ba1ae69c.chunk.js"
+  },
+  {
+    "revision": "c291c55b33f02b900d24",
+    "url": "/otopark_superadmin/static/js/22.0a8f3cec.chunk.js"
+  },
+  {
+    "revision": "9963e20412400ffd2fd1",
+    "url": "/otopark_superadmin/static/js/23.561cbaf6.chunk.js"
+  },
+  {
+    "revision": "616e94191f30c7166dfc",
+    "url": "/otopark_superadmin/static/js/24.274a7fb1.chunk.js"
+  },
+  {
+    "revision": "e5b0f9161d48b965cc8b",
+    "url": "/otopark_superadmin/static/js/25.647fc941.chunk.js"
+  },
+  {
+    "revision": "1e23afe40fe5144e06d3",
+    "url": "/otopark_superadmin/static/js/26.b5305559.chunk.js"
+  },
+  {
+    "revision": "6dd7f636acc5ca508202",
+    "url": "/otopark_superadmin/static/js/27.f9d92bf0.chunk.js"
+  },
+  {
+    "revision": "60baf470c582b2f7e254",
+    "url": "/otopark_superadmin/static/js/3.7fd580a8.chunk.js"
+  },
+  {
+    "revision": "ed4b358043513950ae98",
+    "url": "/otopark_superadmin/static/js/4.3de8b86f.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/otopark_superadmin/static/js/4.3de8b86f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2eed3ad36bf0ffcbb9",
+    "url": "/otopark_superadmin/static/js/5.6904a983.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/otopark_superadmin/static/js/5.6904a983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71bb233cd47d64c83e71",
+    "url": "/otopark_superadmin/static/js/6.067a2619.chunk.js"
+  },
+  {
+    "revision": "7803a2b4c71552e342cade8a7c51e6cd",
+    "url": "/otopark_superadmin/static/js/6.067a2619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4700253434b3104c9c43",
+    "url": "/otopark_superadmin/static/js/9.b155cb50.chunk.js"
+  },
+  {
+    "revision": "57ea35da2f835d94d693d49e80508ef6",
+    "url": "/otopark_superadmin/static/js/9.b155cb50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29525c6e45dcdc573bbf",
+    "url": "/otopark_superadmin/static/js/main.6a8d73a3.chunk.js"
+  },
+  {
+    "revision": "23c18a9380f0acbeb2b8",
+    "url": "/otopark_superadmin/static/js/runtime-main.c1128ac9.js"
+  },
+  {
+    "revision": "39cd4ed62a1f6dcc7a24b0339c61acb3",
+    "url": "/otopark_superadmin/static/media/LoginPageImg.39cd4ed6.svg"
+  },
+  {
+    "revision": "711cdfcc6d3c7d65b9cb881a74f1b4de",
+    "url": "/otopark_superadmin/static/media/LoginPageImg.711cdfcc.png"
+  },
+  {
+    "revision": "2c5a551d888952d87a26eed67622aa16",
+    "url": "/otopark_superadmin/static/media/blue-emailIcon.2c5a551d.svg"
+  },
+  {
+    "revision": "b73142dcf0a438b43ed61b8f52e6cdec",
+    "url": "/otopark_superadmin/static/media/blueBellIcon.b73142dc.svg"
+  },
+  {
+    "revision": "822cf41384e7b4598f25b5e49ff234e5",
+    "url": "/otopark_superadmin/static/media/bookingIcon.822cf413.svg"
+  },
+  {
+    "revision": "a37826a2823a16aa1383ac4939c0eaa9",
+    "url": "/otopark_superadmin/static/media/dashboardAutoIcon.a37826a2.svg"
+  },
+  {
+    "revision": "0a98ca3ef9b6ca8974479ce5f003cff7",
+    "url": "/otopark_superadmin/static/media/dashboardBikeIcon.0a98ca3e.svg"
+  },
+  {
+    "revision": "e6071beda14ae769e6091e726f94c2b7",
+    "url": "/otopark_superadmin/static/media/dashboardCarIcon.e6071bed.svg"
+  },
+  {
+    "revision": "dcd645d2bcd9efc707a99a78734882be",
+    "url": "/otopark_superadmin/static/media/dashboardE-carIcon.dcd645d2.svg"
+  },
+  {
+    "revision": "2388a7730aa9e300e1ddbff75d2a032f",
+    "url": "/otopark_superadmin/static/media/dashboardJeepIcon.2388a773.svg"
+  },
+  {
+    "revision": "5b706ccf4aaa3e2cf6ce46ec46237de2",
+    "url": "/otopark_superadmin/static/media/dashboardTruckIcon.5b706ccf.svg"
+  },
+  {
+    "revision": "d26f5d7b80872247c35b1a26311d8f05",
+    "url": "/otopark_superadmin/static/media/emailIcon.d26f5d7b.svg"
+  },
+  {
+    "revision": "a73a8535d1bf2dd7ec090d08c5efa0c4",
+    "url": "/otopark_superadmin/static/media/growupImg.a73a8535.svg"
+  },
+  {
+    "revision": "11fdbef864345967d8ddfe42e7e1fb52",
+    "url": "/otopark_superadmin/static/media/hardwareANPRIcon.11fdbef8.svg"
+  },
+  {
+    "revision": "0b63cd07b53019d548e3e95000a179c0",
+    "url": "/otopark_superadmin/static/media/hardwareBoomBarrierImg.0b63cd07.svg"
+  },
+  {
+    "revision": "57ad13b2175829b99a2bcb6c548c77d2",
+    "url": "/otopark_superadmin/static/media/hardwareCashBoxImg.57ad13b2.svg"
+  },
+  {
+    "revision": "fd5324fa66603109b5f990f6a2e6c019",
+    "url": "/otopark_superadmin/static/media/hardwareControllerImg.fd5324fa.svg"
+  },
+  {
+    "revision": "7d3a662412e504659a6e1769ce603123",
+    "url": "/otopark_superadmin/static/media/hardwareFareImg.7d3a6624.svg"
+  },
+  {
+    "revision": "bd88848194cf55531e6946e0c3d5daab",
+    "url": "/otopark_superadmin/static/media/hardwareIcon.bd888481.svg"
+  },
+  {
+    "revision": "43b4b2ac8bbddf164ed18ad53defd7fa",
+    "url": "/otopark_superadmin/static/media/hardwareLoopDetectorImg.43b4b2ac.svg"
+  },
+  {
+    "revision": "d12b5abfdced25c7e9231b7856741f9a",
+    "url": "/otopark_superadmin/static/media/hardwarePOSMachineImg.d12b5abf.svg"
+  },
+  {
+    "revision": "3f0debbf9f38c883b737d6752c2d6d0e",
+    "url": "/otopark_superadmin/static/media/hardwarePageImg.3f0debbf.svg"
+  },
+  {
+    "revision": "caa2b9759a38dc7278575ef2c0a861de",
+    "url": "/otopark_superadmin/static/media/hardwarePrinterIcon.caa2b975.svg"
+  },
+  {
+    "revision": "5a03400cdb706725a0ab36a1864146dd",
+    "url": "/otopark_superadmin/static/media/hardwareRFIDIcon.5a03400c.svg"
+  },
+  {
+    "revision": "047d9dd6aa8698d01f1d1c534a444b87",
+    "url": "/otopark_superadmin/static/media/hardwareRFIDImg.047d9dd6.svg"
+  },
+  {
+    "revision": "b5d38e5b9d0ad7512df5be8d1d160471",
+    "url": "/otopark_superadmin/static/media/hardwareScannerImg.b5d38e5b.svg"
+  },
+  {
+    "revision": "c5f331ca75eb5b7ca682fba7d3e97e7f",
+    "url": "/otopark_superadmin/static/media/hardwareServerImg.c5f331ca.svg"
+  },
+  {
+    "revision": "b16784f98866da8a9d3ca35c046bd221",
+    "url": "/otopark_superadmin/static/media/hardwareTicketDispenserIcon.b16784f9.svg"
+  },
+  {
+    "revision": "16c08b7acb70f508eeb12524ab8b48eb",
+    "url": "/otopark_superadmin/static/media/hardwareTicketMachineImg.16c08b7a.svg"
+  },
+  {
+    "revision": "c485935db8a4a6ae099982fb334d524c",
+    "url": "/otopark_superadmin/static/media/homeIcon.c485935d.svg"
+  },
+  {
+    "revision": "09cce601313def0f682004938899692b",
+    "url": "/otopark_superadmin/static/media/indianRupeeIcon.09cce601.svg"
+  },
+  {
+    "revision": "415a0727dfa7ee341aab8f9386879add",
+    "url": "/otopark_superadmin/static/media/menu-barCloseIcon.415a0727.svg"
+  },
+  {
+    "revision": "99b6a2323e104512e03019e2841f59b1",
+    "url": "/otopark_superadmin/static/media/menu-barIcon.99b6a232.svg"
+  },
+  {
+    "revision": "944f5ddaf59186b49663c027fcc7dc01",
+    "url": "/otopark_superadmin/static/media/monthlyPassIcon.944f5dda.svg"
+  },
+  {
+    "revision": "dae460011189a40ca71043626c6318e4",
+    "url": "/otopark_superadmin/static/media/mumbai.dae46001.png"
+  },
+  {
+    "revision": "54a7a6db8bd501b7866bf1b196921a78",
+    "url": "/otopark_superadmin/static/media/notificationIcon.54a7a6db.svg"
+  },
+  {
+    "revision": "cc7c1ca7625814e88df7d5df2c9b39f5",
+    "url": "/otopark_superadmin/static/media/parkingIcon.cc7c1ca7.svg"
+  },
+  {
+    "revision": "fdd71a776296d2557fa27c5944446d23",
+    "url": "/otopark_superadmin/static/media/paymentIcon.fdd71a77.svg"
+  },
+  {
+    "revision": "4e63b6de46b1faf06b9a8b7a2404d17f",
+    "url": "/otopark_superadmin/static/media/question-markIcon.4e63b6de.svg"
+  },
+  {
+    "revision": "37a009190207a921ea21a31475546adc",
+    "url": "/otopark_superadmin/static/media/supportIcon.37a00919.svg"
+  },
+  {
+    "revision": "f650ba34d9804ed6183ed6f79a2f6c3b",
+    "url": "/otopark_superadmin/static/media/userIcon.f650ba34.svg"
+  },
+  {
+    "revision": "32718227c07b496e7efc81100ce98f68",
+    "url": "/otopark_superadmin/static/media/whiteBellIcon.32718227.svg"
+  },
+  {
+    "revision": "42bcebb3a44c7aea3e32d18eb3221676",
+    "url": "/otopark_superadmin/static/media/whiteBookingIcon.42bcebb3.svg"
+  },
+  {
+    "revision": "0bbcae1b348f1915bc3d65fac36f1f00",
+    "url": "/otopark_superadmin/static/media/whiteMenu-barCloseIcon.0bbcae1b.svg"
+  }
+]);
